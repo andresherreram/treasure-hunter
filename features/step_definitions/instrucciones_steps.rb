61b5d5051e39979ad65f331bbfb71007d4410474
kleer <kleer@kleer-csd-box>
2017-02-03 10:05:10 -0500

    Instructions

--- features/step_definitions/instrucciones_steps.rb
+++ features/step_definitions/instrucciones_steps.rb
@@ -5,5 +5,8 @@ end
 
 Then(/^debo ver las instrucciones$/) do
   expect(page.body).to match /Instrucciones/m
+	expect(page.body).to match /a: avanzar/m
+	expect(page.body).to match /d: derecha/m
+	expect(page.body).to match /i: izquierda/m
 end
 
