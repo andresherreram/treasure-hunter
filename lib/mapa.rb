d0ded1c8d403552667177ed25b928db78e0bc107
kleer <kleer@kleer-csd-box>
2017-02-03 12:06:35 -0500

    trabajando en avanzar

--- lib/mapa.rb
+++ lib/mapa.rb
@@ -1,16 +1,39 @@
+class Personaje
+
+	attr_reader :direccion
+
+	def initialize
+		@direccion = :abajo
+	end
+
+	def to_s
+		"P"
+	end
+
+	alias :inspect :to_s
+end
+
 class Mapa
 	attr_reader :cuadricula
+
 
 	def initialize
 		@cuadricula = []
 		8.times do
 			@cuadricula << (0...8).map{}
 		end
-		@cuadricula[0][0] = "P"
+		@cuadricula[0][0] = Personaje.new
 	end
 	
 	def ejecutar(comando)
+		case comando
+		when "a"
+			avanzar()
+		end
 		@cuadricula[0][0] = nil
 		@cuadricula[0][1] = "P"
+	end
+
+	def avanzar
 	end
 end
